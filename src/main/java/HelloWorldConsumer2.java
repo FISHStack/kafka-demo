eab23236fca8de083ba21bbf5908494b1e17fc36
lyp-home <liangyuanpengem@163.com>
2020-02-14 10:51:36 +0800

    change  topic to kafeidou

--- src/main/java/HelloWorldConsumer2.java
+++ src/main/java/HelloWorldConsumer2.java
@@ -12,18 +12,19 @@ public class HelloWorldConsumer2 {
   public static void main(String[] args) {
     Properties props = new Properties();
 
-    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.3.137:9092");
-    props.put(ConsumerConfig.GROUP_ID_CONFIG ,"test") ;
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.1.60:9092");
+    props.put(ConsumerConfig.GROUP_ID_CONFIG ,"kafeidou_group") ;
     props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
     props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
     props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
     props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+    props.put("auto.offset.reset", "earliest");
 
     Consumer<String, String> consumer = new KafkaConsumer<>(props);
-    consumer.subscribe(Arrays.asList("page_visits"));
+    consumer.subscribe(Arrays.asList("kafeidou2"));
 
     while (true) {
-      ConsumerRecords<String, String> records = consumer.poll(100);
+      ConsumerRecords<String, String> records = consumer.poll(1000);
       for (ConsumerRecord<String, String> record : records) {
         System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
       }
--- src/main/java/HelloWorldProducer.java
+++ src/main/java/HelloWorldProducer.java
@@ -1,6 +1,8 @@
 import java.util.Date;
 import java.util.Properties;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 import kafka.producer.KeyedMessage;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
@@ -16,7 +18,7 @@ public class HelloWorldProducer {
     Random rnd = new Random();
 
     Properties props = new Properties();
-    props.put("bootstrap.servers", "192.168.3.137:9092");
+    props.put("bootstrap.servers", "192.168.1.60:9092");
     props.put("acks", "all");
     props.put("retries", 0);
     props.put("batch.size", 16384);
@@ -25,16 +27,19 @@ public class HelloWorldProducer {
     props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
     props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
     //配置partitionner选择策略，可选配置
-    props.put("partitioner.class", "SimplePartitioner2");
+//    props.put("partitioner.class", "SimplePartitioner2");
+    props.put("message.timeout.ms", "3000");
 
     Producer<String, String> producer = new KafkaProducer<>(props);
+
+    String topic = "kafeidou";
 
     for (long nEvents = 0; nEvents < events; nEvents++) {
       long runtime = new Date().getTime();
       String ip = "192.168.2." + rnd.nextInt(255);
       String msg = runtime + ",www.example.com," + ip;
       System.out.println(msg);
-      ProducerRecord<String, String> data = new ProducerRecord<String, String>("page_visits", ip, msg);
+      ProducerRecord<String, String> data = new ProducerRecord<String, String>(topic, ip, msg);
       producer.send(data,
           new Callback() {
             public void onCompletion(RecordMetadata metadata, Exception e) {
@@ -46,6 +51,8 @@ public class HelloWorldProducer {
             }
           });
     }
+    System.out.println("send message done");
     producer.close();
+    System.exit(-1);
   }
 }
